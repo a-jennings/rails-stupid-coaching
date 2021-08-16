5c195e5019e15ad4fc5945c6408bf85803826198
Drew Jennings <andrew_jennings@ymail.com>
2021-08-16 11:52:41 +0100

    basic logic of app complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @user_input = params[:ask]
+    if @user_input == 'I am going to work'
+      @coach_output = 'Great!'
+    elsif @user_input.include? '?'
+      @coach_output = 'Silly question, get dressed and go to work!'
+    else
+      @coach_output = "I don't care, get dressed and go to work!"
+    end
+    params[:output] = @coach_output
+  end
 end
